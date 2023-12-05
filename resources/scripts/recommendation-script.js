c5b1e3b4df69f97b53631ef330988e3a140cc22d
Akhyar Ahmed <nuhasahmed007@gmail.com>
2023-12-05 15:40:22 +0100

    Updates!

--- resources/scripts/recommendation-script.js
+++ resources/scripts/recommendation-script.js
@@ -6,8 +6,13 @@
 // });
 
 // JavaScript to toggle the visibility of the recommendation letter
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('toggleLetter').addEventListener('click', function () {
-        document.getElementById('letterContainer').style.display = 'block';
-    });
+document.getElementById('toggleLetter').addEventListener('click', function () {
+    // Assuming 'your_pdf_file.pdf' is the name of your PDF file
+    window.open('resources/pdfs/LOR_Akhyar_DFKI.pdf', '_blank');
+});
+
+// JavaScript to toggle the visibility of the recommendation letter
+document.getElementById('toggleLetter_1').addEventListener('click', function () {
+    // Assuming 'your_pdf_file.pdf' is the name of your PDF file
+    window.open('resources/pdfs/LOR_Akhyar_DFKI.pdf', '_blank');
 });
