db4ed5cad98f2e3e2cebfa27b1069740e87280b7
Akhyar Ahmed <nuhasahmed007@gmail.com>
2023-12-06 01:41:03 +0100

    More Updates!

--- resources/scripts/projects-script.js
+++ resources/scripts/projects-script.js
@@ -2,47 +2,64 @@
 const pdfPaths = [
     'resources/pdfs/project_1.pdf',
     'resources/pdfs/project_2.pdf',
-    'resources/pdfs/project_3.pdf'
+    'resources/pdfs/project_3.pdf',
+    'resources/pdfs/project_4.pdf'
     ];
 
     // Function to open the PDF in an iframe
     function openPdfInIframe(pdfUrl) {
-    const iframe = document.createElement('iframe');
-    iframe.src = pdfUrl;
-    iframe.width = '60%';
-    iframe.height = '80%';
+        const iframe = document.createElement('iframe');
+        iframe.src = pdfUrl;
+        iframe.width = '60%';
+        iframe.height = '80%';
 
-    // Create a modal overlay
-    const modalOverlay = document.createElement('div');
-    modalOverlay.style.position = 'fixed';
-    modalOverlay.style.top = '0';
-    modalOverlay.style.left = '0';
-    modalOverlay.style.width = '100%';
-    modalOverlay.style.height = '100%';
-    modalOverlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-    modalOverlay.style.display = 'flex';
-    modalOverlay.style.alignItems = 'center';
-    modalOverlay.style.justifyContent = 'center';
-    modalOverlay.appendChild(iframe);
+        // Create a modal overlay
+        const modalOverlay = document.createElement('div');
+        modalOverlay.style.position = 'fixed';
+        modalOverlay.style.top = '0';
+        modalOverlay.style.left = '0';
+        modalOverlay.style.width = '100%';
+        modalOverlay.style.height = '100%';
+        modalOverlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+        modalOverlay.style.display = 'flex';
+        modalOverlay.style.alignItems = 'center';
+        modalOverlay.style.justifyContent = 'center';
 
-    // Append the overlay to the body
-    document.body.appendChild(modalOverlay);
+        modalOverlay.appendChild(iframe);
 
-    // Close the modal on click outside the iframe
-    modalOverlay.addEventListener('click', () => {
-        document.body.removeChild(modalOverlay);
-    });
+        // Append the overlay to the body
+        document.body.appendChild(modalOverlay);
+
+        // Close the modal on click outside the iframe
+        modalOverlay.addEventListener('click', () => {
+            document.body.removeChild(modalOverlay);
+        });
     }
 
     document.addEventListener('DOMContentLoaded', function () {
-    // Attach the function to the buttons
-    pdfPaths.forEach((pdfPath, index) => {
-        const buttonId = `openPdfButton${index + 1}`;
-        const openPdfButton = document.getElementById(buttonId);
-        if (openPdfButton) {
-        openPdfButton.addEventListener('click', () => openPdfInIframe(pdfPath));
-        } else {
-        console.error(`Button with id ${buttonId} not found.`);
-        }
-    });
+        // Attach the function to the buttons
+        pdfPaths.forEach((pdfPath, index) => {
+            const buttonId = `openPdfButton${index + 1}`;
+            const openPdfButton = document.getElementById(buttonId);
+            if (openPdfButton) {
+            openPdfButton.addEventListener('click', () => openPdfInIframe(pdfPath));
+            } else {
+            console.error(`Button with id ${buttonId} not found.`);
+            }
+        });
+
+        // ---------------------- Event handler for project-5 -------------- //
+        // var imageCarousel = new bootstrap.Carousel(document.getElementById('imageCarousel'));
+        // // Add an event listener to the "See Demonstration" button
+        // var openPdfButton = document.getElementById('openPdfButton5');
+        // if (openPdfButton) {
+        // openPdfButton.addEventListener('click', function () {
+        //     // Manually trigger the modal
+        //     var myModal = new bootstrap.Modal(document.getElementById('project5Modal'));
+        //     myModal.show();
+        // });
+        // } else {
+        //     console.error('Button with id openPdfButton4 not found.');
+        // }
+        // ---------------------------- END ------------------------------ //
     });
